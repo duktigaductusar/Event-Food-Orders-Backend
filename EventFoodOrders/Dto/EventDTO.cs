1d30929ccf792adc87cfe0a399a779fb27277cf7
Max Sjögren <max.sjogren@ductus.se>
2024-12-09 08:23:47 +0100

    Added Action and Description. Replased getters and setters with { get; set; }

--- EventFoodOrders/Dto/EventDTO.cs
+++ EventFoodOrders/Dto/EventDTO.cs
@@ -2,38 +2,10 @@
 
 public class EventDTO
 {
-    private Guid eventId;
-    private String EventName { get; set; }
-    private DateTime EventDate { get; set; }
+    public Guid eventId { get; set; }
+    public String EventName { get; set; }
+    public DateTime EventDate { get; set; }
+    public String Description { get; set; }
+    public bool Action { get; set; }
 
-    public Guid getEventId()
-    {
-        return eventId;
-    }
-
-    public void setEventId(Guid eventId)
-    {
-        this.eventId = eventId;
-    }
-
-    public String getEventName()
-    {
-        return EventName;
-    }
-
-    public void setEventName(String eventName)
-    {
-        this.EventName = eventName;
-    }
-
-    public DateTime getEventDate()
-    {
-        return EventDate;
-
-    }
-
-    public void setEventDate(DateTime eventDate)
-    {
-        this.EventDate = eventDate;
-    }
 }
