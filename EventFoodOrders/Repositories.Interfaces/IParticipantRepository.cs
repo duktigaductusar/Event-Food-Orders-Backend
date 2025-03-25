8392df7a1624b42c25f3a6e6d0c1422346e6613d
danmicha <daniel.michanech@ductus.se>
2025-03-25 16:22:25 +0100

    Added code documentation to all interfaces.

--- EventFoodOrders/Repositories.Interfaces/IParticipantRepository.cs
+++ EventFoodOrders/Repositories.Interfaces/IParticipantRepository.cs
@@ -1,14 +1,51 @@
 ﻿using EventFoodOrders.Entities;
 
-namespace EventFoodOrders.Repositories.Interfaces
+namespace EventFoodOrders.Repositories.Interfaces;
+
+/// <summary>
+/// Interacts with the Participant table in the database through the Participant DbSet in the DbContext.
+/// </summary>
+public interface IParticipantRepository
 {
-    public interface IParticipantRepository
-    {
-        Participant AddParticipant(Participant participant);
-        void DeleteParticipant(Guid participantId);
-        IEnumerable<Participant> GetAllParticipantsForUser(Guid userId);
-        Participant? GetParticipantWithParticipantId(Guid participantId);
-        Participant? GetParticipantWithUserId(Guid userId);
-        Participant UpdateParticipant(Guid participantId, Participant updatedParticipant);
-    }
+    /// <summary>
+    /// Adds a participant to the database.
+    /// </summary>
+    /// <param name="participant"></param>
+    /// <returns></returns>
+    Participant AddParticipant(Participant participant);
+
+    /// <summary>
+    /// Deletes a participant in the database.
+    /// </summary>
+    /// <param name="participantId"></param>
+    void DeleteParticipant(Guid participantId);
+
+    /// <summary>
+    /// Gets all the participants with a given user Id.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    IEnumerable<Participant> GetAllParticipantsForUser(Guid userId);
+
+    /// <summary>
+    /// Gets a participant using that participant's Id.
+    /// </summary>
+    /// <param name="participantId"></param>
+    /// <returns></returns>
+    Participant? GetParticipantWithParticipantId(Guid participantId);
+
+    /// <summary>
+    /// Gets a participant using that participant's user Id.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    Participant? GetParticipantWithUserId(Guid userId);
+
+    /// <summary>
+    /// Updates a participant in the database.
+    /// </summary>
+    /// <param name="participantId"></param>
+    /// <param name="updatedParticipant"></param>
+    /// <returns></returns>
+    Participant UpdateParticipant(Guid participantId, Participant updatedParticipant);
 }
--- EventFoodOrders/Services.Interfaces/IEventService.cs
+++ EventFoodOrders/Services.Interfaces/IEventService.cs
@@ -2,21 +2,46 @@
 
 namespace EventFoodOrders.Services.Interfaces;
 
-// Handles getting, creating, and updating events using both event and participant repositories.
+/// <summary>
+/// Handles getting, creating, and updating events using both event and participant repositories.
+/// </summary>
 public interface IEventService
 {
-    // Creates an event using a Dto with information. Also creates an owner participant and any other invited participants.
+    /// <summary>
+    /// Creates an event using a Dto with information. Also creates an owner participant and any other invited participants.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="eventForCreation"></param>
+    /// <returns></returns>
     EventForResponseDto CreateEvent(Guid userId, EventForCreationDto eventForCreation);
 
-    // Deletes an event.
+    /// <summary>
+    /// Deletes an event.
+    /// </summary>
+    /// <param name="eventId"></param>
+    /// <returns></returns>
     bool DeleteEvent(Guid eventId);
 
-    // Gets all the events that a user has been invited to.
+    /// <summary>
+    /// Gets all the events that a user has been invited to.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
     IEnumerable<EventForResponseDto> GetAllEventsForUser(Guid userId);
 
-    // Gets a single event, given that the user has been invited to it.
+    /// <summary>
+    /// Gets a single event, given that the user has been invited to it.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="eventId"></param>
+    /// <returns></returns>
     EventForResponseWithDetailsDto GetEventForUser(Guid userId, Guid eventId);
 
-    // Updates an event given its Id and a Dto.
+    /// <summary>
+    /// Updates an event given its Id and a Dto.
+    /// </summary>
+    /// <param name="eventId"></param>
+    /// <param name="updatedEventDto"></param>
+    /// <returns></returns>
     EventForResponseDto UpdateEvent(Guid eventId, EventForUpdateDto updatedEventDto);
 }
