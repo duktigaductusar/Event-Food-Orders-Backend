d03f98114b5a67eaece566cad4946c0880a1f8bf
danmicha <daniel.michanech@ductus.se>
2025-03-31 09:56:34 +0200

    Changed name of User controller.

--- EventFoodOrders/Controllers/UserController.cs
+++ EventFoodOrders/Controllers/UserController.cs
@@ -5,13 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace EventFoodOrders.Controllers;
 
 [ApiController]
-[Route("api/[Controller]")]
+[Route("api/user")]
 public class UserController(IUserService userService)
 {
     private readonly IUserService _userService = userService;
 
     [HttpGet]
-    [Route("users")]
     public ActionResult<List<UserDto>> GetUsers(string queryString)
     {
         var users = _userService.GetUsers(queryString);
